5bbcceca87bcb76a81c2bbc556e32f8464e27948
Minierva Maaya <maayachamcham@gmail.com>
2025-08-24 16:45:53 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world application

---
+++ OneDrive/Document/new JavaScript/javascript-course-main/01-fundamentals-part-1/starter/script.js
@@ -0,0 +1,57 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log(js);
+ 
+console.log("====   VARIABLES ====");
+ 
+let firstName = "cham";
+console.log(firstName);
+ 
+ 
+let age = 20;
+console.log(age);
+age = 50;
+console.log(age);
+ 
+const birthYear = 2001;
+console.log(birthYear);
+ 
+const PI = 3.1415
+console.log(PI);
+ 
+var job = "programmer";
+job = "Bantay sa Comshop";
+console.log(job);
+ 
+const country = "KOREA";
+const language = "tagalog";
+ 
+age = 24;
+age = 25;
+ 
+console.log("=== DATA TYPES ===");
+ 
+let id =987654;
+console.log(id);
+console.log(typeof id);
+ 
+let lastName = "Maaya";
+console.log(lastName);
+console.log(typeof lastName);
+ 
+let javaScriptIsFun = "true";
+console.log.apply(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+ 
+let year;
+console.log(year);
+console.log(typeof year);
+ 
+ 
+let dynamicVariable = 40;
+console.log(dynamicVariable, typeof dynamicVariable);
+ 
+dynamicVariable ="i am hungry ";
+console.log(dynamicVariable, typeof dynamicVariable);
+ 
+    
